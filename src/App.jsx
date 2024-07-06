b308eea913baca2768c03ca8e03f33fe3e29af32
MakariiSlupskyi <makariislupskyi@gmail.com>
2024-07-06 12:22:09 +0300

    react basics

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+// import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Food />
+      <Food />
+      <Footer />
+    </>
+  );
+}
+
+export default App
