de1b393ebe469b165d6280526779d5682106f609
MakariiSlupskyi <makariislupskyi@gmail.com>
2024-07-08 10:48:23 +0300

    created components dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import OnChangeForm from './OnChangeForm.jsx'
-// import './App.css'
+import Button from './components/Button.jsx'
+import Card from './components/Card.jsx'
+import Counter from './components/Counter.jsx'
+import List from './components/List.jsx'
+import OnChangeForm from './components/OnChangeForm.jsx'
+import ProfilePicture from './components/ProfilePicture.jsx'
 
 function App() {
   return (
